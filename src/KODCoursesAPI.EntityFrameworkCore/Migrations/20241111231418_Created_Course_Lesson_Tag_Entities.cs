83a14b66a638f214c818929c663beb51c1cda799
MisterCat100 <savingleb2007@mail.ru>
2024-11-12 02:21:22 +0300

    feat(Migrations): make new migration with new tables

---
+++ src/KODCoursesAPI.EntityFrameworkCore/Migrations/20241111231418_Created_Course_Lesson_Tag_Entities.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KODCoursesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Created_Course_Lesson_Tag_Entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppCourses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Description = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    TotalDurationMinutes = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppCourses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppTags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    Description = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppTags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppLessons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Description = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    DurationMinutes = table.Column<int>(type: "integer", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppLessons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppLessons_AppCourses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "AppCourses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LessonTag",
+                columns: table => new
+                {
+                    LessonsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LessonTag", x => new { x.LessonsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_LessonTag_AppLessons_LessonsId",
+                        column: x => x.LessonsId,
+                        principalTable: "AppLessons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LessonTag_AppTags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "AppTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppLessons_CourseId",
+                table: "AppLessons",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LessonTag_TagsId",
+                table: "LessonTag",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LessonTag");
+
+            migrationBuilder.DropTable(
+                name: "AppLessons");
+
+            migrationBuilder.DropTable(
+                name: "AppTags");
+
+            migrationBuilder.DropTable(
+                name: "AppCourses");
+        }
+    }
+}
