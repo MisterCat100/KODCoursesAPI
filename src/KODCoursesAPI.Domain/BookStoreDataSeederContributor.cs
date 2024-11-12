d69efa98927a845869baa9174abec1ea0fe8d226
MisterCat100 <savingleb2007@mail.ru>
2024-11-12 04:59:07 +0300

    feat(SeederContributor): add start values to the db

--- src/KODCoursesAPI.Domain/BookStoreDataSeederContributor.cs
+++ src/KODCoursesAPI.Domain/BookStoreDataSeederContributor.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using KODCoursesAPI.Books;
+using KODCoursesAPI.Entities;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
-using KODCoursesAPI.Books;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Guids;
+using Volo.Abp.MultiTenancy;
 
 namespace KODCoursesAPI;
 
@@ -11,10 +15,27 @@ public class KODCoursesAPIDataSeederContributor
     : IDataSeedContributor, ITransientDependency
 {
     private readonly IRepository<Book, Guid> _bookRepository;
+    private readonly IRepository<Course, Guid> _courseRepository;
+    private readonly IRepository<Lesson, Guid> _lessonRepository;
+    private readonly IRepository<Tag, Guid> _tagRepository;
 
-    public KODCoursesAPIDataSeederContributor(IRepository<Book, Guid> bookRepository)
+    private readonly IGuidGenerator _guidGenerator;
+    private readonly ICurrentTenant _currentTenant;
+
+    public KODCoursesAPIDataSeederContributor(IRepository<Book, Guid> bookRepository,
+                                              IRepository<Course, Guid> courseRepository,
+                                              IRepository<Lesson, Guid> lessonRepository,
+                                              IRepository<Tag, Guid> tagRepository,
+                                              IGuidGenerator guidGenerator,
+                                              ICurrentTenant currentTenant)
     {
         _bookRepository = bookRepository;
+        _courseRepository = courseRepository;
+        _lessonRepository = lessonRepository;
+        _tagRepository = tagRepository;
+
+        _guidGenerator = guidGenerator;
+        _currentTenant = currentTenant;
     }
 
     public async Task SeedAsync(DataSeedContext context)
@@ -43,5 +64,41 @@ public class KODCoursesAPIDataSeederContributor
                 autoSave: true
             );
         }
+
+
+        Tag tag = new(_guidGenerator.Create())
+        {
+            Name = "C#",
+            Description = "C#, .NET"
+        };
+
+        Lesson lesson = new(_guidGenerator.Create())
+        {
+            Title = "№1: What is .NET?",
+            Description = "Explain .NET",
+            Tags = [tag],
+            DurationMinutes = 17
+        };
+
+        Course course = new(_guidGenerator.Create())
+        {
+            Name = "Learn C#",
+            Description = "You will learn basic syntax and ASP.NET Core",
+            Lessons = [lesson]
+        };
+
+
+        tag.Lessons.Add(lesson);
+        lesson.Course = course;
+        course.TotalDurationMinutes = course.Lessons.Sum(l => l.DurationMinutes);
+
+        if (await _courseRepository.GetCountAsync() <= 0)
+            await _courseRepository.InsertAsync(course, autoSave: true);
+
+        if (await _lessonRepository.GetCountAsync() <= 0)
+            await _lessonRepository.InsertAsync(lesson, autoSave: true);
+
+        if (await _tagRepository.GetCountAsync() <= 0)
+            await _tagRepository.InsertAsync(tag, autoSave: true);
     }
 }
