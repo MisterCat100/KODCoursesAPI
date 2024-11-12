45124e1682b7f0343f920b27f715d5455879da5d
MisterCat100 <savingleb2007@mail.ru>
2024-11-12 06:01:09 +0300

    feat(AutoMapper): configure AutoMapper

--- src/KODCoursesAPI.Application/KODCoursesAPIApplicationAutoMapperProfile.cs
+++ src/KODCoursesAPI.Application/KODCoursesAPIApplicationAutoMapperProfile.cs
@@ -1,5 +1,8 @@
 using AutoMapper;
 using KODCoursesAPI.Books;
+using KODCoursesAPI.Dtos;
+using KODCoursesAPI.Dtos.CreateUpdateDtos;
+using KODCoursesAPI.Entities;
 
 namespace KODCoursesAPI;
 
@@ -9,6 +12,17 @@ public class KODCoursesAPIApplicationAutoMapperProfile : Profile
     {
         CreateMap<Book, BookDto>();
         CreateMap<CreateUpdateBookDto, Book>();
+
+
+        CreateMap<Course, CourseDto>();
+        CreateMap<CreateUpdateCourseDto, Course>();
+
+        CreateMap<Lesson, LessonDto>();
+        CreateMap<CreateUpdateLessonDto, Lesson>();
+
+        CreateMap<Tag, TagDto>();
+        CreateMap<CreateUpdateTagDto, Tag>();
+
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
